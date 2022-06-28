ced5bd682e06d8193c5861714cdf886f70d7c39a
Ashutosh Singh Kushwaha <63491073+Kushashu-1@users.noreply.github.com>
2022-06-28 17:55:26 +0530

    updated main.py

--- main.py
+++ main.py
@@ -54,15 +54,15 @@ def load_heart_disease_dataset():
 
 # ----------------------------Navigation Bar-------------------------------------#
 
-st.sidebar.title("Disease Prediction and Diagnostic Analysis Using Deep Learning")
+st.sidebar.title("Disease Prediction and Attribute Analyzation Using Machine Learning")
 page = st.sidebar.radio(" ",('Home' ,'Dataset Analysis' ,'About'))
 
 #----------------------------------About Page-----------------------------------#
 if page=='About':
     st.title("About Project and Creator")
     st.write(
-        'We approached this project to learn about the Core Concept of the Deep learning environment Under the Guidance : Dr. Rohit Gupta Sir . With this in mind,first we learned about different datasets and algorithms of deeplearning.'
-        'I would like to say that Machine Learning has inspired me for doing great things by learning about great things this project is one of my starters project in this domain and with it I am able to experience not only life of an Enginner but a Physican as well.'
+        'We approached this project to learn about the Core Concept of the Machine learning environment Under the Guidance : Dr. Rohit Sir . With this in mind,first we learned about different datasets and algorithms of Machine learning.'
+        'I would like to say that Machine Learning has inspired me for doing this Project . This project is one of my starters project in this domain and with it I am able to experience not only life of an Enginner but a Physican as well.'
         'For Better User Interface experience and designs we taken help from Streamlit which is open source app framework in Python language.'
     )
     st.subheader('Aim')
@@ -73,7 +73,7 @@ if page=='About':
     st.subheader('Datasets Used :')
     st.write('1 . Breast Cancer Dataset : Worldwide, breast cancer is the most common type of cancer in women and the second highest in terms of mortality rates.Diagnosis of breast cancer is performed when an abnormal lump is found (from self-examination or x-ray) or a tiny speck of calcium is seen(on an x-ray). After a suspicious lump is found, the doctor will conduct a diagnosis to determine whether it is cancerous and,if so, whether it has spread to other parts of the body.This breast cancer dataset was obtained from the University of Wisconsin Hospitals, Madison from Dr. William H. Wolberg.')
     st.write('For more Information : https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29')
-    st.write('2 . Cardiovascular Disease dataset : The dataset consists of 70 000 records of patients data in 12 features, such as age, gender, systolic blood pressure, diastolic blood pressure, and etc. The target class "cardio" equals to 1, when patient has cardiovascular desease, and its 0 if patient is healthy.')
+    st.write('2 . Cardiovascular Disease dataset : The dataset consists of 70,000 records of patients data in 12 features, such as age, gender, systolic blood pressure, diastolic blood pressure, and etc. The target class "cardio" equals to 1, when patient has cardiovascular desease, and its 0 if patient is healthy.')
     st.write('For More Information : https://www.kaggle.com/datasets/sulianova/cardiovascular-disease-dataset')
     st.write('3 . This dataset is originally from the National Institute of Diabetes and Digestive and Kidney Diseases. All the patients here are female 21 years or older.It contains the following columns: Pregnancies: Number of times pregnant Glucose: Plasma glucose concentration a 2 hours in an oral glucose tolerance test BloodPressure: Diastolic blood pressure (mm Hg) SkinThickness: Triceps skin fold thickness (mm)Insulin: 2-Hour serum insulin (mu U/ml) BMI: Body mass index (weight in kg/(height in m)^2)DiabetesPedigreeFunction: Diabetes pedigree function Age: Age (years) Outcome: Class variable (0 or 1)')
     st.write('For more Information :https://www.kaggle.com/datasets/mathchi/diabetes-data-set')
